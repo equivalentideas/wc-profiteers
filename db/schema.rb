456932b3082533b7e273f377adf7f868c4e7a695
Luke Bacon <luke@equivalentideas.com>
2015-05-16 13:38:55 +1000

    be rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510055529) do
+ActiveRecord::Schema.define(version: 20150516032844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150510055529) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "abn"
+    t.string   "website"
   end
 
   add_index "contractors", ["abn"], name: "index_contractors_on_abn", using: :btree
