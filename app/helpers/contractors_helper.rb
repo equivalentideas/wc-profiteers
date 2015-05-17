e5a078311f77cb4738abe39638f9c8e24278ab9c
Luke Bacon <luke@equivalentideas.com>
2015-05-17 23:21:33 +1000

    Add helper for formatting urls to just the domain

--- app/helpers/contractors_helper.rb
+++ app/helpers/contractors_helper.rb
@@ -1,2 +1,5 @@
 module ContractorsHelper
+  def simple_url(url)
+    url.sub(/^https*:\/\//, '').sub('www.', '').sub(/\/\S*$/, '')
+  end
 end
--- spec/helpers/contractors_helper_spec.rb
+++ spec/helpers/contractors_helper_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 describe ContractorsHelper do
+  describe "#simple_url" do
+    it { expect(helper.simple_url("http://example.com")).to eq "example.com" }
+    it { expect(helper.simple_url("http://example.com.au")).to eq "example.com.au" }
+    it { expect(helper.simple_url("http://example2.com")).to eq "example2.com" }
+    it { expect(helper.simple_url("https://example.com")).to eq "example.com" }
+    it { expect(helper.simple_url("http://www.example.com")).to eq "example.com" }
+    it { expect(helper.simple_url("http://example.com/")).to eq "example.com" }
+    it { expect(helper.simple_url("http://example.com/foo")).to eq "example.com" }
+    it { expect(helper.simple_url("http://example.com/foo.php")).to eq "example.com" }
+    it { expect(helper.simple_url("http://example.com/foo/bar.php")).to eq "example.com" }
+  end
 end
