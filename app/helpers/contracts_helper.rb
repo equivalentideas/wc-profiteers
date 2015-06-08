36a977ab79638cbba569454eaa86571fdeef94b6
Luke Bacon <luke@equivalentideas.com>
2015-06-09 00:34:01 +1000

    Use new helper to clean up contract descriptions

--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -1,6 +1,6 @@
 module ContractsHelper
   def link_to_contract_by_id(id)
-    link_to(h(Contract.find(id).description), "##{h(Contract.find(id).can_id)}")
+    link_to(h(Contract.find(id).display_description), "##{h(Contract.find(id).can_id)}")
   end
 
   def new_contracts_sentence(create_versions)
