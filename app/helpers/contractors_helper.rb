d65b61b7f1cf9eddb5a9aed176443231a5f76dc2
Luke Bacon <luke@equivalentideas.com>
2015-06-08 15:03:23 +1000

    Add method for displaying sentence listing newly added contractors

--- app/helpers/contractors_helper.rb
+++ app/helpers/contractors_helper.rb
@@ -2,4 +2,9 @@ module ContractorsHelper
   def simple_url(url)
     url.sub(/^https*:\/\//, '').sub('www.', '').sub(/\/\S*$/, '')
   end
+
+  def new_contractors_sentence(create_versions)
+    contractors = create_versions.map {|v| content_tag(:em, Contractor.find(v.item_id).name)}
+    return "#{pluralize(contractors.count, 'new contractor')} #{contractors.to_sentence} added."
+  end
 end
--- spec/helpers/contractors_helper_spec.rb
+++ spec/helpers/contractors_helper_spec.rb
@@ -12,4 +12,28 @@ describe ContractorsHelper do
     it { expect(helper.simple_url("http://example.com/foo.php")).to eq "example.com" }
     it { expect(helper.simple_url("http://example.com/foo/bar.php")).to eq "example.com" }
   end
+
+  describe "#new_contractors_sentence" do
+    let!(:contractor1) { Contractor.create id: 1, name: "Foo Company", abn: "123" }
+    let!(:contractor2) { Contractor.create id: 2, name: "Bar Company", abn: "456" }
+    let!(:contractor3) { Contractor.create id: 3, name: "Wiz Company", abn: "789" }
+    let(:version1) { PaperTrail::Version.new id: 1, item_type: "Contractor", item_id: 1, event: "create" }
+    let(:version2) { PaperTrail::Version.new id: 2, item_type: "Contractor", item_id: 2, event: "create" }
+    let(:version3) { PaperTrail::Version.new id: 3, item_type: "Contractor", item_id: 3, event: "create" }
+
+    context "There is one new contractor" do
+      let(:versions) {[version1]}
+      it { expect(helper.new_contractors_sentence(versions)).to eq "1 new contractor <em>Foo Company</em> added."}
+    end
+
+    context "There are two new contractors" do
+      let(:versions) {[version1, version2]}
+      it { expect(helper.new_contractors_sentence(versions)).to eq "2 new contractors <em>Foo Company</em> and <em>Bar Company</em> added."}
+    end
+
+    context "There are three new contractors" do
+      let(:versions) {[version1, version2, version3]}
+      it { expect(helper.new_contractors_sentence(versions)).to eq "3 new contractors <em>Foo Company</em>, <em>Bar Company</em>, and <em>Wiz Company</em> added."}
+    end
+  end
 end
