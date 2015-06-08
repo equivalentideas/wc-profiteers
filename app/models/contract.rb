c54637688e755366e11fe6edf8497fcd786d7551
Luke Bacon <luke@equivalentideas.com>
2015-06-09 00:29:29 +1000

    Add method for cutting repetitive prefixes from descriptions

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -6,4 +6,14 @@ class Contract < ActiveRecord::Base
   validates :can_id, uniqueness: true
 
   default_scope {order('start_date DESC')}
+
+  def display_description
+    if description[0..12] == 'WestConnex - '
+      description.sub('WestConnex - ', '')
+    elsif description[0..5] == 'WDA - '
+      description.sub('WDA - ', '')
+    else
+      description
+    end
+  end
 end
---
+++ spec/models/contract_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe Contract do
+  let!(:contract) { Contract.create id: 1, can_id: '123' }
+
+  context 'description starts with "WestConnex - "' do
+    before :each do
+      contract.update description: 'WestConnex - Foo Service'
+    end
+
+    it '#display_description' do
+      expect(contract.display_description).to eq 'Foo Service'
+    end
+  end
+
+  context 'description starts with "WDA - "' do
+    before :each do
+      contract.update description: 'WDA - Foo Service'
+    end
+
+    it '#display_description' do
+      expect(contract.display_description).to eq 'Foo Service'
+    end
+  end
+
+  context 'description contains but doesnt start with "WestConnex - "' do
+    before :each do
+      contract.update description: 'Foo Service WestConnex - '
+    end
+
+    it '#display_description' do
+      expect(contract.display_description).to eq 'Foo Service WestConnex - '
+    end
+  end
+
+  context 'description does not contain "WestConnex - " or "WDA - "' do
+    before :each do
+      contract.update description: 'Foo Service'
+    end
+
+    it '#display_description' do
+      expect(contract.display_description).to eq 'Foo Service'
+    end
+  end
+end
