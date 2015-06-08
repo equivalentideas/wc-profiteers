b1b580a139170851127f911810d28d3b1e4b2af7
Luke Bacon <luke@equivalentideas.com>
2015-06-08 16:19:27 +1000

    Add links to new contractors sentence

--- app/helpers/contractors_helper.rb
+++ app/helpers/contractors_helper.rb
@@ -4,7 +4,7 @@ module ContractorsHelper
   end
 
   def new_contractors_sentence(create_versions)
-    contractors = create_versions.map {|v| content_tag(:em, h(Contractor.find(v.item_id).name))}
+    contractors = create_versions.map {|v| link_to(h(Contractor.find(v.item_id).name), "##{h(Contractor.find(v.item_id).abn)}")}
     return "#{'New contractor'.pluralize(contractors.count)} #{contractors.to_sentence} added.".html_safe
   end
 end
