9289101fec67c7abbdab3a1b18d0e235a483cfc9
Luke Bacon <luke@equivalentideas.com>
2015-06-08 16:02:01 +1000

    Add very basic list of contractors add grouped by day

--- app/controllers/contractors_controller.rb
+++ app/controllers/contractors_controller.rb
@@ -1,5 +1,7 @@
 class ContractorsController < ApplicationController
   def index
     @contractors = Contractor.all.sort { |a, b| b.total_est_contract_value <=> a.total_est_contract_value }
+    @contractor_history = PaperTrail::Version.where("created_at > ? and item_type = ? and event = ?", 2.week.ago, "Contractor", "create")
+    @last_two_weeks = (2.weeks.ago.to_date..Date.today).to_a.reverse
   end
 end
