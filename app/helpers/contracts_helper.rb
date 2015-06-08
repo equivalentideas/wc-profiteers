36b3226318295f1e42c4c17f557c551fe9a5a344
Luke Bacon <luke@equivalentideas.com>
2015-06-08 22:47:36 +1000

    Add helper for listing new contracts

--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -2,4 +2,9 @@ module ContractsHelper
   def link_to_contract_by_id(id)
     link_to(h(Contract.find(id).description), "##{h(Contract.find(id).can_id)}")
   end
+
+  def new_contracts_sentence(create_versions)
+    contracts = create_versions.map {|v| link_to_contract_by_id(v.item_id)}
+    return "#{'New contract'.pluralize(contracts.count)} #{contracts.to_sentence} added.".html_safe
+  end
 end
--- spec/helpers/contracts_helper_spec.rb
+++ spec/helpers/contracts_helper_spec.rb
@@ -10,4 +10,29 @@ describe ContractsHelper do
       it { expect(helper.link_to_contract_by_id(id)).to be_html_safe }
     end
   end
+
+  describe '#new_contracts_sentence' do
+    let!(:contract1) { Contract.create id: 1, description: 'Foo Service', can_id: '123' }
+    let!(:contract2) { Contract.create id: 2, description: 'Bar Service', can_id: '456' }
+    let!(:contract3) { Contract.create id: 3, description: 'Wiz Service', can_id: '789' }
+    let(:version1) { PaperTrail::Version.new id: 1, item_type: 'Contract', item_id: 1, event: 'create' }
+    let(:version2) { PaperTrail::Version.new id: 2, item_type: 'Contract', item_id: 2, event: 'create' }
+    let(:version3) { PaperTrail::Version.new id: 3, item_type: 'Contract', item_id: 3, event: 'create' }
+
+    context 'there is one new contract' do
+      let(:versions) {[version1]}
+      it { expect(helper.new_contracts_sentence(versions)).to eq 'New contract <a href="#123">Foo Service</a> added.'}
+      it { expect(helper.new_contracts_sentence(versions)).to be_html_safe }
+    end
+
+    context 'there are two new contracts' do let(:versions) {[version1, version2]}
+      it { expect(helper.new_contracts_sentence(versions)).to eq 'New contracts <a href="#123">Foo Service</a> and <a href="#456">Bar Service</a> added.'}
+      it { expect(helper.new_contracts_sentence(versions)).to be_html_safe }
+    end
+
+    context 'there are three new contracts' do let(:versions) {[version1, version2, version3]}
+      it { expect(helper.new_contracts_sentence(versions)).to eq 'New contracts <a href="#123">Foo Service</a>, <a href="#456">Bar Service</a>, and <a href="#789">Wiz Service</a> added.'}
+      it { expect(helper.new_contracts_sentence(versions)).to be_html_safe }
+    end
+  end
 end
