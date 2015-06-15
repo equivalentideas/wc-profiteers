9f958e25ce2b3e756eca4b3c689f9f9b69d9a3a4
Luke Bacon <luke@equivalentideas.com>
2015-06-16 08:25:54 +1000

    Remove date query from version collections

--- app/controllers/contractors_controller.rb
+++ app/controllers/contractors_controller.rb
@@ -1,8 +1,8 @@
 class ContractorsController < ApplicationController
   def index
     @contractors = Contractor.all.sort { |a, b| b.total_est_contract_value <=> a.total_est_contract_value }
-    @contractor_history = PaperTrail::Version.where("created_at > ? and item_type = ? and event = ?", 2.week.ago, "Contractor", "create")
-    @contract_history = PaperTrail::Version.where("created_at > ? and item_type = ? and event = ?", 2.week.ago, "Contract", "create")
+    @contractor_history = PaperTrail::Version.where("item_type = ? and event = ?", "Contractor", "create")
+    @contract_history = PaperTrail::Version.where("item_type = ? and event = ?", "Contract", "create")
     @days_with_changes = PaperTrail::Version.group_by_day(:created_at).keys.map{|k| k.to_date}.reverse
   end
 end
