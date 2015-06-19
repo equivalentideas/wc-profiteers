3b8afa1620e209b8abf888ec029f4663163b4554
Luke Bacon <luke@equivalentideas.com>
2015-06-20 08:23:46 +1000

    Only get days with changes we want to display

--- app/controllers/contractors_controller.rb
+++ app/controllers/contractors_controller.rb
@@ -3,6 +3,10 @@ class ContractorsController < ApplicationController
     @contractors = Contractor.all.sort { |a, b| b.total_est_contract_value <=> a.total_est_contract_value }
     @contractor_history = PaperTrail::Version.where("item_type = ? and event = ?", "Contractor", "create")
     @contract_history = PaperTrail::Version.where("item_type = ? and event = ?", "Contract", "create")
-    @days_with_changes = PaperTrail::Version.group_by_day(:created_at).keys.map{|k| k.to_date}.reverse
+
+    # create a Array of days with contracts/contractors created or value changed.
+    @days_with_changes = @contractor_history.group_by_day(:created_at).keys.map{|k| k.to_date}
+    @days_with_changes.insert @contract_history.group_by_day(:created_at).keys.map{|k| k.to_date}
+    @days_with_changes = @days_with_changes.sort { |a,b| b <=> a }[0..2]
   end
 end
