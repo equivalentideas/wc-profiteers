fbff6d2d7341d6d5a3e34c7b8f52510b8075f582
Luke Bacon <luke@equivalentideas.com>
2015-07-14 16:43:58 -0700

    Add description column with bundle exec rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612133354) do
+ActiveRecord::Schema.define(version: 20150714225916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150612133354) do
     t.datetime "updated_at"
     t.string   "abn"
     t.string   "website"
+    t.text     "description"
   end
 
   add_index "contractors", ["abn"], name: "index_contractors_on_abn", using: :btree
