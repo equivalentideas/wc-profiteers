ce66422ae35ea6d83dd28bef357e9267397487b7
Luke Bacon <luke@equivalentideas.com>
2015-09-10 01:56:38 +1000

    Add method for importing contractors from a csv

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -6,6 +6,24 @@ class Contractor < ActiveRecord::Base
   validates :name, :abn, presence: true
   validates :abn, uniqueness: true
 
+  def self.import_contractors_from_csv(csv_path)
+    require 'CSV'
+    csv = CSV.parse(
+      File.read(csv_path),
+      headers: true
+    )
+
+    csv.each do |row|
+      if Contractor.find_by(abn: row["abn"]).nil?
+        Contractor.create(
+          name: row['name'],
+          abn: row['abn'],
+          acn: row['acn']
+        )
+      end
+    end
+  end
+
   def total_est_contract_value
     contracts.sum(:value)
   end
--- spec/models/contractor_spec.rb
+++ spec/models/contractor_spec.rb
@@ -38,4 +38,39 @@ describe Contractor do
       expect(contractor.corporate_id).to eq 'no_corporate_id_1'
     end
   end
+
+  describe '.import_contractors_from_csv' do
+    before :each do
+      csv_path = 'spec/csv_examples/contractors.csv'
+
+      Contractor.import_contractors_from_csv(csv_path)
+    end
+
+    context "when csv includes a contractor that isn't in the database" do
+      new_contractor = {
+        name: "Allens Linklaters",
+        abn: "21001295843",
+        acn: "001295843"
+      }
+
+      it "creates new contractors if we don’t have them already" do
+        expect(
+          Contractor.find_by(abn: new_contractor[:abn]).name
+        )
+        .to eq new_contractor[:name]
+      end
+    end
+
+    context "when csv includes an existing contractor" do
+      old_contractor_abn = "50098008818"
+      old_contractor = Contractor.create id: 2, name: "ADVISIAN PTY LTD", abn: old_contractor_abn, updated_at: 7.days.ago
+
+      it "doesn’t update the existing contractor" do
+        expect(
+          Contractor.find_by(abn: old_contractor_abn).updated_at.to_date
+        )
+        .to eq 7.days.ago.to_date
+      end
+    end
+  end
 end
