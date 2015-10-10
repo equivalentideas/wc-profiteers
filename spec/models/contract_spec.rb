63cd9fa07ff57d569eea6dd37f0eccd118c85425
Luke Bacon <luke@equivalentideas.com>
2015-10-10 18:55:17 +1100

    Use wrapping describe block to simplify tests (refactor only)

--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -3,53 +3,45 @@ require 'rails_helper'
 describe Contract do
   let!(:contract) { Contract.create id: 1, can_id: '123' }
 
-  context 'description starts with no caps "westconnex - "' do
-    before :each do
-      contract.update description: 'westconnex - Foo Service'
+  describe '#display_description' do
+    context 'description starts with no caps "westconnex - "' do
+      before :each do
+        contract.update description: 'westconnex - Foo Service'
+      end
+
+      it { expect(contract.display_description).to eq 'Foo Service' }
     end
 
-    it '#display_description' do
-      expect(contract.display_description).to eq 'Foo Service'
-    end
-  end
+    context 'description starts with capticalised "WestConnex - "' do
+      before :each do
+        contract.update description: 'WestConnex - Foo Service'
+      end
 
-  context 'description starts with capticalised "WestConnex - "' do
-    before :each do
-      contract.update description: 'WestConnex - Foo Service'
+      it { expect(contract.display_description).to eq 'Foo Service' }
     end
 
-    it '#display_description' do
-      expect(contract.display_description).to eq 'Foo Service'
-    end
-  end
+    context 'description starts with "WDA - "' do
+      before :each do
+        contract.update description: 'WDA - Foo Service'
+      end
 
-  context 'description starts with "WDA - "' do
-    before :each do
-      contract.update description: 'WDA - Foo Service'
+      it { expect(contract.display_description).to eq 'Foo Service' }
     end
 
-    it '#display_description' do
-      expect(contract.display_description).to eq 'Foo Service'
-    end
-  end
+    context 'description contains but doesnt start with "WestConnex - "' do
+      before :each do
+        contract.update description: 'Foo Service WestConnex - '
+      end
 
-  context 'description contains but doesnt start with "WestConnex - "' do
-    before :each do
-      contract.update description: 'Foo Service WestConnex - '
+      it { expect(contract.display_description).to eq 'Foo Service WestConnex - ' }
     end
 
-    it '#display_description' do
-      expect(contract.display_description).to eq 'Foo Service WestConnex - '
-    end
-  end
+    context 'description does not contain "WestConnex - " or "WDA - "' do
+      before :each do
+        contract.update description: 'Foo Service'
+      end
 
-  context 'description does not contain "WestConnex - " or "WDA - "' do
-    before :each do
-      contract.update description: 'Foo Service'
-    end
-
-    it '#display_description' do
-      expect(contract.display_description).to eq 'Foo Service'
+      it { expect(contract.display_description).to eq 'Foo Service' }
     end
   end
 end
