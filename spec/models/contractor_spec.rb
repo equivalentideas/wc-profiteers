e8f032e046173463516dfb8d9cacc82c840ab500
Luke Bacon <luke@equivalentideas.com>
2015-10-10 15:37:36 +1100

    Use consistent single quotes

--- spec/models/contractor_spec.rb
+++ spec/models/contractor_spec.rb
@@ -20,17 +20,17 @@ describe Contractor do
   end
 
   describe '.import_contractors_from_csv' do
-    context "when csv includes a new contractor" do
+    context 'when csv includes a new contractor' do
       before { Contractor.import_contractors_from_csv('spec/csv_examples/contractors.csv') }
 
-      it "creates new contractors if we don’t have them already" do
+      it 'creates new contractors if we don’t have them already' do
         new_contractor_abn = '1234567890'
 
         expect(Contractor.find_by(abn: new_contractor_abn).abn).to eq new_contractor_abn
       end
     end
 
-    context "when csv includes an existing contractor" do
+    context 'when csv includes an existing contractor' do
       before :each do
         @old_contractor_abn = '1234567890'
         create(:contractor, abn: @old_contractor_abn, updated_at: 7.days.ago)
@@ -38,7 +38,7 @@ describe Contractor do
         Contractor.import_contractors_from_csv('spec/csv_examples/contractors.csv')
       end
 
-      it "doesn’t update the existing contractor" do
+      it 'doesn’t update the existing contractor' do
         expect(Contractor.find_by(abn: @old_contractor_abn).updated_at.to_date)
           .to eq 7.days.ago.to_date
       end
