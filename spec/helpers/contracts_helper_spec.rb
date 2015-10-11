14ecc0fdc5f272c864fd6685a31dbe73cc0628a1
Luke Bacon <luke@equivalentideas.com>
2015-10-11 14:48:53 +1100

    Don't use the can_id to link to contracts.
    Change ahead of importing contracts without unique ids.

--- spec/helpers/contracts_helper_spec.rb
+++ spec/helpers/contracts_helper_spec.rb
@@ -6,7 +6,7 @@ describe ContractsHelper do
 
     describe '#link_to_contract_by_id' do
       let(:id) {1}
-      it { expect(helper.link_to_contract_by_id(id)).to eq '<a href="#123">Foo Service</a>' }
+      it { expect(helper.link_to_contract_by_id(id)).to eq "<a href=\"#contract_#{contract1.id}\">Foo Service</a>" }
       it { expect(helper.link_to_contract_by_id(id)).to be_html_safe }
     end
   end
@@ -21,17 +21,17 @@ describe ContractsHelper do
 
     context 'there is one new contract' do
       let(:versions) {[version1]}
-      it { expect(helper.new_contracts_sentence(versions)).to eq 'New contract: <a href="#123">Foo Service</a>.'}
+      it { expect(helper.new_contracts_sentence(versions)).to eq "New contract: #{helper.link_to_contract_by_id(contract1.id)}."}
       it { expect(helper.new_contracts_sentence(versions)).to be_html_safe }
     end
 
     context 'there are two new contracts' do let(:versions) {[version1, version2]}
-      it { expect(helper.new_contracts_sentence(versions)).to eq 'New contracts: <a href="#123">Foo Service</a> and <a href="#456">Bar Service</a>.'}
+      it { expect(helper.new_contracts_sentence(versions)).to eq "New contracts: #{helper.link_to_contract_by_id(contract1.id)} and #{helper.link_to_contract_by_id(contract2.id)}."}
       it { expect(helper.new_contracts_sentence(versions)).to be_html_safe }
     end
 
     context 'there are three new contracts' do let(:versions) {[version1, version2, version3]}
-      it { expect(helper.new_contracts_sentence(versions)).to eq 'New contracts: <a href="#123">Foo Service</a>, <a href="#456">Bar Service</a>, and <a href="#789">Wiz Service</a>.'}
+      it { expect(helper.new_contracts_sentence(versions)).to eq "New contracts: #{helper.link_to_contract_by_id(contract1.id)}, #{helper.link_to_contract_by_id(contract2.id)}, and #{helper.link_to_contract_by_id(contract3.id)}."}
       it { expect(helper.new_contracts_sentence(versions)).to be_html_safe }
     end
   end
