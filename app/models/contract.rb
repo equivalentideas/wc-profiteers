e3611a31df061286085f9f292f434b5a055640d7
Luke Bacon <luke@equivalentideas.com>
2015-10-11 16:53:22 +1100

    Extract variable

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -10,10 +10,12 @@ class Contract < ActiveRecord::Base
   def self.import_contracts_from_morph(with_debug_output: nil)
     require 'open-uri'
 
+    morph_url_base = "https://api.morph.io/equivalentideas/westconnex_contracts/data.json?key=#{ENV['MORPH_SECRET_KEY']}&query="
+
     Contractor.all.each do |contractor|
-      url = "https://api.morph.io/equivalentideas/westconnex_contracts/data.json?key=#{ENV['MORPH_SECRET_KEY']}&query=select%20contracts%20from%20'contractors'%20where%20abn%3D'#{contractor.abn}'"
+      url = morph_url_base + "select%20contracts%20from%20'contractors'%20where%20abn%3D'#{contractor.abn}'"
       JSON.parse(open(url).read)[0]["contracts"].split(", ").each do |c|
-        url = "https://api.morph.io/equivalentideas/westconnex_contracts/data.json?key=#{ENV['MORPH_SECRET_KEY']}&query=select%20*%20from%20'contracts'%20where%20contract_award_notice_id%3D'#{c}'"
+        url = morph_url_base + "select%20*%20from%20'contracts'%20where%20contract_award_notice_id%3D'#{c}'"
         contract_data = JSON.parse(open(url).read)[0]
 
         if with_debug_output
