dbb639ac4cb964495c3c2b3508c3e22544fc6669
Luke Bacon <luke@equivalentideas.com>
2015-10-11 15:01:11 +1100

    Remove explicit return

--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -5,6 +5,6 @@ module ContractsHelper
 
   def new_contracts_sentence(create_versions)
     contracts = create_versions.map {|v| link_to_contract(v.item)}
-    return "#{'New contract'.pluralize(contracts.count)}: #{contracts.to_sentence}.".html_safe
+    "#{'New contract'.pluralize(contracts.count)}: #{contracts.to_sentence}.".html_safe
   end
 end
