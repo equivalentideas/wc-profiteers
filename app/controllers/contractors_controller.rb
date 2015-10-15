6f05ea53d8c112e975e0a805efe1445992d4f31d
Luke Bacon <luke@equivalentideas.com>
2015-10-16 07:57:02 +1100

    Extract logic to controller

--- app/controllers/contractors_controller.rb
+++ app/controllers/contractors_controller.rb
@@ -4,6 +4,9 @@ class ContractorsController < ApplicationController
     @contractor_history = PaperTrail::Version.where("item_type = ? and event = ?", "Contractor", "create")
     @contract_history = PaperTrail::Version.where("item_type = ? and event = ?", "Contract", "create")
 
+    @contract_count = Contract.count
+    @contract_total_value = Contract.sum(:value)
+
     # create a Array of days with contracts/contractors created or value changed.
     @days_with_changes = @contractor_history.group_by_day(:created_at).keys.map{|k| k.to_date}
     @days_with_changes.insert @contract_history.group_by_day(:created_at).keys.map{|k| k.to_date}
