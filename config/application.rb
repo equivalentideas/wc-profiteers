9b3106797593c2a864649abc233ba4b74ee0c33f
Luke Bacon <luke@equivalentideas.com>
2018-02-13 15:43:30 +1100

    Opt-in to silence warnings

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,10 @@ module WcProfiteers
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    #
+    # DEPRECATION WARNING: Currently, Active Record suppresses errors raised within `after_rollback`/`after_commit` callbacks and only print them to the logs. In the next version, these errors will no longer be suppressed. Instead, the errors will propagate normally just like in other Active Record callbacks.
+    # You can opt into the new behavior and remove this warning by setting:
+    # config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
